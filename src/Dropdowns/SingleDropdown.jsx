1656887e2e8978ff18b74a696b1c6c0505f194a3
Mohit <mohit.joshi@capanicus.com>
2023-09-18 13:08:21 +0530

    changes

--- src/Dropdowns/SingleDropdown.jsx
+++ src/Dropdowns/SingleDropdown.jsx
@@ -3,28 +3,44 @@ import '../assets/SingleDropdown.css';
 import { BsChevronDown } from 'react-icons/bs';
 import { RxCross2 } from 'react-icons/rx';
 import { FiLoader } from 'react-icons/fi';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useClickOutsideHandler from './useClickOutsideHandler';
-let observer;
-const SingleDropdown = ({ queryKey = 'listing', queryFn = null, value = '', onChange = () => {}, placeholder = 'Select option', displayKeyWithLabel = false }) => {
+const SingleDropdown = ({ queryKey = 'listing', queryFn = null, value = '', onChange = () => {}, placeholder = 'Select option', displayKeyWithLabel = false, isRawData = false, rawData = null }) => {
   const dropdownRef = useRef(null);
+  const [length, setLength] = useState(10);
   const [isOpen, setIsOpen] = useState(false);
   const [search, setSearch] = useState('');
   const [listData, setListData] = useState([]);
-  const { data, isSuccess, isError, isLoading, isFetching } = useQuery({
+
+  const {
+    data: apiData,
+    isSuccess,
+    isError,
+    isLoading,
+    isFetching,
+  } = useQuery({
     queryKey: [queryKey, length],
     queryFn: () => queryFn({ length }),
     select: (data) => data?.data?.data,
     refetchOnWindowFocus: false,
-    enabled: Boolean(queryFn),
+    enabled: Boolean(queryFn) && !isRawData,
     retry: 0,
   });
+
   useClickOutsideHandler(dropdownRef, () => setIsOpen(false));
+
   useEffect(() => {
-    if (Array.isArray(data) && data.length > 0 && isSuccess) {
-      setListData(data);
+    if (Array.isArray(apiData) && apiData.length > 0 && isSuccess) {
+      setListData(apiData);
     }
-  }, [isSuccess, data]);
+  }, [isSuccess, apiData]);
+
+  useEffect(() => {
+    if (isRawData && Array.isArray(rawData) && rawData.length > 0) {
+      setListData(rawData);
+    }
+  }, [isRawData, rawData]);
+
   return (
     <div className='dropdown' ref={dropdownRef}>
       <div
@@ -56,8 +72,27 @@ const SingleDropdown = ({ queryKey = 'listing', queryFn = null, value = '', onCh
           {isError && <div className='no-records'>Error while getting list</div>}
           {(isLoading || isFetching) && <FiLoader className='dropdown-loader' />}
           {(!isLoading || !isFetching) && !isError && Array.isArray(listData) && listData.length === 0 && <div className='no-records'>No records</div>}
-          {!search && Array.isArray(listData) && listData.length > 0 && listData.map(({ value, label }, index) => <DropdownListItem {...{ index, label, value, displayKeyWithLabel, onChange }} key={index} />)}
-          {search && Array.isArray(listData) && listData.length > 0 && listData.filter(({ value, label }) => label.toLowerCase().includes(search.toLowerCase()) || value.includes(search)).map(({ value, label }, index) => <DropdownListItem {...{ index, label, value, displayKeyWithLabel, onChange }} key={index} />)}
+          {!search && Array.isArray(listData) && listData.length > 0 && listData.slice(0, length).map(({ value, label }, index, arr) => <DropdownListItem {...{ index, label, value, displayKeyWithLabel, onChange, arr }} key={index} />)}
+          {search &&
+            Array.isArray(listData) &&
+            listData.length > 0 &&
+            listData
+              .filter(({ value, label }) => label.toLowerCase().includes(search.toLowerCase()) || value.includes(search))
+              .map(({ value, label }, index) => {
+                return (
+                  <div
+                    key={index}
+                    className='dropdown-list-item'
+                    onClick={() => {
+                      onChange(value);
+                    }}
+                  >
+                    <p>
+                      {label} {displayKeyWithLabel ? `(${value})` : ''}
+                    </p>
+                  </div>
+                );
+              })}
         </div>
       )}
     </div>
@@ -66,7 +101,7 @@ const SingleDropdown = ({ queryKey = 'listing', queryFn = null, value = '', onCh
 
 export default SingleDropdown;
 
-const DropdownListItem = ({ index, label, value, displayKeyWithLabel, onChange }) => {
+const DropdownListItem = ({ index, label, arr, value, displayKeyWithLabel, onChange }) => {
   return (
     <div
       key={index}
